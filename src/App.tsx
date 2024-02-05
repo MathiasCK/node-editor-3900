40f593da24f1d03e82004b2c42b81750d005a9c7
hskaftun <s364776@oslomet.no>
2024-02-05 13:47:15 +0100

    Update App.tsx
    Lagt til mulighet til å legge til linjer som både er hele og stripete. Lagt til knapper som kan lage de 7 ulike typene linjer for MVP.

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,11 @@ import { INITIAL_EDGES, INITIAL_NODES, buttonVariants } from "./config";
 import { NodeType } from "./types";
 
 export default function App() {
+  const [relation, setRelation] = useState({
+    color: "white", 
+    strokeDasharray: false
+  }); 
+
   const nodeTypes = { block: Block, connector: Connector, terminal: Terminal };
 
   const [nodes, setNodes, onNodesChange] = useNodesState(INITIAL_NODES);
@@ -25,10 +30,14 @@ export default function App() {
   const onConnect = useCallback(
     (params: Edge | Connection) => {
       if (canConnect(params)) {
-        return setEdges(eds => addEdge(params, eds));
+        const newConnection ={
+          ...params,
+          style:{stroke:relation.color, strokeWidth: 2, strokeDasharray: relation.strokeDasharray ? "5, 5" : ""},
+    }
+        return setEdges(eds => addEdge(newConnection, eds));
       }
     },
-    [setEdges],
+    [setEdges, relation.color, relation.strokeDasharray],
   );
 
   const addNode = (type: NodeType) => {
@@ -49,6 +58,37 @@ export default function App() {
 
   return (
     <main className="w-screen h-screen bg-black">
+      <div className="h-10 min-w-10 absolute right-0 bottom-50 flex flex-col z-20"> 
+      <button 
+          className={buttonVariants.block} 
+          onClick={() => setRelation({color: "green", strokeDasharray: false})}>Part of
+          </button>
+          <button 
+          className={buttonVariants.block} 
+          onClick={() => setRelation({color: "lightblue", strokeDasharray: false})}>Connected to
+          </button>
+          <button 
+          className={buttonVariants.block} 
+          onClick={() => setRelation({color: "blue", strokeDasharray: false})}>Transfer to
+          </button>
+          <button 
+          className={buttonVariants.block} 
+          onClick={() => setRelation({color: "brown", strokeDasharray: false})}>Specialisation of
+          </button>
+          <button 
+          className={buttonVariants.block} 
+          onClick={() => setRelation({color: "brown", strokeDasharray: false})}>Fulfilled by
+          </button>
+          <button 
+          className={buttonVariants.block} 
+          onClick={() => setRelation({color: "whitesmoke", strokeDasharray: true})}>Proxy
+          </button>
+          <button 
+          className={buttonVariants.block} 
+          onClick={() => setRelation({color: "whitesmoke", strokeDasharray: true})}>Projection
+          </button>
+          </div>
+
       <div className="absolute z-10 flex justify-center w-full">
         <button
           className={buttonVariants.block}
