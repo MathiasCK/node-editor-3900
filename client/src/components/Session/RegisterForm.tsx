91eac77043effc4b3ec42414f5bc14640b60d78d
hskaftun <s364776@oslomet.no>
2024-02-29 18:09:16 +0100

    Lagd RegisterForm og henvisninger til denne i App.tsx og LoginForm #2

--- client/src/components/Session/RegisterForm.tsx
+++ client/src/components/Session/RegisterForm.tsx
@@ -1,25 +1,25 @@
-import { useState, SyntheticEvent } from "react";
-import { Navigate} from 'react-router-dom';
+import { useState, SyntheticEvent } from 'react';
+import { Navigate } from 'react-router-dom';
 
 const RegisterForm = () => {
-    const [name, setName] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [navigate, setNavigate] = useState(false);
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [navigate, setNavigate] = useState(false);
 
-    //Stored in Local Storage before backe-end is implemented. 
-    const submit = async (e: SyntheticEvent) => {
-        e.preventDefault();
+  //Stored in Local Storage before backe-end is implemented.
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
 
-        console.log({
-            name,
-            username,
-            password
-        })
-        setNavigate(true);
-    };
+    console.log({
+      name,
+      username,
+      password,
+    });
+    setNavigate(true);
+  };
 
-    /* This is the code for when the back-end/Database will be implemented:
+  /* This is the code for when the back-end/Database will be implemented:
     const submit = async (e: SyntheticEvent) => {
         e.preventDefault();
 
@@ -36,34 +36,59 @@ const RegisterForm = () => {
         setNavigate(true);
     } */
 
-    //Navigation when clicking submit back to LoginForm
-    if (navigate) {
-        return <Navigate to="/loginform"/>;
-    } 
+  //Navigation when clicking submit back to LoginForm
+  if (navigate) {
+    return <Navigate to="/loginform" />;
+  }
 
   return (
-    <div className="flex justify-center items-center h-screen bg-indigo-600">
-        <div className='w-96 p-6 shadow-lg bg-white rounded-md'>
-            <form onSubmit={submit}>
-                <h1 className='text-3xl block text-center text-black font-bold'><i className='fa-solid fa-user'></i>Register</h1>
-                <div className="mt-3">
-                    <input type='text' id='name' className='border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600 text-black' placeholder='Name' required
-                        onChange={e => setName(e.target.value)} />
-                </div>
-                <div className='mt-3'>
-                    <input type='text' id='username' className='border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600 text-black' placeholder='Username' required
-                        onChange={e => setUsername(e.target.value)} />
-                </div>
-                <div className='mt-3'>
-                    <input type='password' id='password' className='border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600 text-black' placeholder='Password' required
-                        onChange={e => setPassword(e.target.value)} />
-                </div>
+    <div className="flex h-screen items-center justify-center bg-indigo-600">
+      <div className="w-96 rounded-md bg-white p-6 shadow-lg">
+        <form onSubmit={submit}>
+          <h1 className="block text-center text-3xl font-bold text-black">
+            <i className="fa-solid fa-user"></i>Register
+          </h1>
+          <div className="mt-3">
+            <input
+              type="text"
+              id="name"
+              className="w-full border px-2 py-1 text-base text-black focus:border-gray-600 focus:outline-none focus:ring-0"
+              placeholder="Name"
+              required
+              onChange={e => setName(e.target.value)}
+            />
+          </div>
+          <div className="mt-3">
+            <input
+              type="text"
+              id="username"
+              className="w-full border px-2 py-1 text-base text-black focus:border-gray-600 focus:outline-none focus:ring-0"
+              placeholder="Username"
+              required
+              onChange={e => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="mt-3">
+            <input
+              type="password"
+              id="password"
+              className="w-full border px-2 py-1 text-base text-black focus:border-gray-600 focus:outline-none focus:ring-0"
+              placeholder="Password"
+              required
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
 
-                <div className='mt-5'>
-                    <button type='submit' className='border-2 border-indigo-700 bg-indigo-700 text-white py-1 w-full rounded-md hover:bg-transparent hover:text-indigo-700 font-semibold"><i class="fa-solid fa-right-to-bracket'>Submit</button>
-                </div>
-            </form>
-        </div>
+          <div className="mt-5">
+            <button
+              type="submit"
+              className='font-semibold"><i class="fa-solid fa-right-to-bracket w-full rounded-md border-2 border-indigo-700 bg-indigo-700 py-1 text-white hover:bg-transparent hover:text-indigo-700'
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
