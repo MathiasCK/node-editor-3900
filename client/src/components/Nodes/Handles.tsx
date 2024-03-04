1ca528ca340baaa32d50264ef3c11a30e927c7c6
Mathias Kolberg <Mathias.ck2000@gmail.com>
2024-03-04 11:46:14 +0100

    Create more target & source handles

--- client/src/components/Nodes/Handles.tsx
+++ client/src/components/Nodes/Handles.tsx
@@ -6,11 +6,45 @@ const Handles: FC<{
 }> = ({ nodeId }) => {
   return (
     <>
-      <Handle type="target" position={Position.Top} id={`${nodeId}_target`} />
+      <Handle
+        type="target"
+        position={Position.Top}
+        id={`${nodeId}_top_target`}
+      />
+      <Handle
+        type="source"
+        position={Position.Top}
+        id={`${nodeId}_top_source`}
+      />
+      <Handle
+        type="target"
+        position={Position.Bottom}
+        id={`${nodeId}_bottom_target`}
+      />
       <Handle
         type="source"
         position={Position.Bottom}
-        id={`${nodeId}_source`}
+        id={`${nodeId}_bottom_source`}
+      />
+      <Handle
+        type="target"
+        position={Position.Left}
+        id={`${nodeId}_left_target`}
+      />
+      <Handle
+        type="source"
+        position={Position.Left}
+        id={`${nodeId}_left_source`}
+      />
+      <Handle
+        type="target"
+        position={Position.Right}
+        id={`${nodeId}_right_target`}
+      />
+      <Handle
+        type="source"
+        position={Position.Right}
+        id={`${nodeId}_right_source`}
       />
     </>
   );
