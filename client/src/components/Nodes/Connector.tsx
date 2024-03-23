4ce1b696ae69d05c5b24f95287c027645f8631e6
Mathias Kolberg <Mathias.ck2000@gmail.com>
2024-03-23 11:58:43 +0100

    Add tooltip with custom name for terminals & connectors

--- client/src/components/Nodes/Connector.tsx
+++ client/src/components/Nodes/Connector.tsx
@@ -1,27 +1,46 @@
 import Handles from './Handles';
 import { useSidebar } from '@/hooks';
 import type { CustomNodeProps } from '@/lib/types';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '../ui/tooltip';
 
 const Connector = (props: CustomNodeProps) => {
   const { openSidebar } = useSidebar();
 
   return (
-    <figure id={props.data.label}>
-      <div
-        onClick={() => openSidebar(props)}
-        className={`h-8 w-8 rounded-xl bg-${props.data.aspect}-light dark:bg-${props.data.aspect}-dark`}
-      >
-        <header className="flex h-full w-full items-center justify-center">
-          <p
-            className={`text-center text-${props.data.aspect}-foreground-light dark:text-${props.data.aspect}-foreground-dark`}
-          >
-            {props.data.customName === '' ? props.id : props.data.customName}
-          </p>
-        </header>
-      </div>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <figure id={props.data.label}>
+            <div
+              onClick={() => openSidebar(props)}
+              className={`h-8 w-8 rounded-xl bg-${props.data.aspect}-light dark:bg-${props.data.aspect}-dark`}
+            >
+              <header className="flex h-full w-full items-center justify-center">
+                <p
+                  className={`text-center text-${props.data.aspect}-foreground-light dark:text-${props.data.aspect}-foreground-dark`}
+                >
+                  {props.id}
+                </p>
+              </header>
+            </div>
 
-      <Handles nodeId={props.data.label} />
-    </figure>
+            <Handles nodeId={props.data.label} />
+          </figure>
+        </TooltipTrigger>
+        {props.data.customName !== '' && (
+          <TooltipContent>
+            <p className="text-xs text-gray-500 dark:text-gray-400">
+              {props.data.customName}
+            </p>
+          </TooltipContent>
+        )}
+      </Tooltip>
+    </TooltipProvider>
   );
 };
 
