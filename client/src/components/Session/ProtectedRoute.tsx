MathiasCK/node-editor

Fått inn noe Session elementer #2
Sett noen videoer og fått inn redirect fra user til login-side. Login-side er foreløpig bare applikasjonen.

client/src/components/Session/ProtectedRoute.tsx
@@ -1,0 +1,21 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+const ProtectedRoutes = () => {
+	const localStorageToken = localStorage.getItem("token");
+
+	// decode the jwt & see if its expired
+	const decodedToken = localStorageToken
+		? JSON.parse(atob(localStorageToken.split(".")[1]))
+		: null;
+	const isExpired = decodedToken ? decodedToken.exp < Date.now() / 1000 : true;
+
+	// if token is expired, remove it
+	if (isExpired) {
+		localStorage.removeItem("token");
+		return <Navigate to="/login" replace />;
+	}
+
+	return <Outlet />;
+};
+
+export default ProtectedRoutes;

MathiasCK/node-editor-3900

client/src/components/Session/ProtectedRoute.tsx
@@ -1,21 +1,21 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = () => {
-	const localStorageToken = localStorage.getItem("token");
+  const localStorageToken = localStorage.getItem('token');
 
-	// decode the jwt & see if its expired
-	const decodedToken = localStorageToken
-		? JSON.parse(atob(localStorageToken.split(".")[1]))
-		: null;
-	const isExpired = decodedToken ? decodedToken.exp < Date.now() / 1000 : true;
+  // decode the jwt & see if its expired
+  const decodedToken = localStorageToken
+    ? JSON.parse(atob(localStorageToken.split('.')[1]))
+    : null;
+  const isExpired = decodedToken ? decodedToken.exp < Date.now() / 1000 : true;
 
-	// if token is expired, remove it
-	if (isExpired) {
-		localStorage.removeItem("token");
-		return <Navigate to="/loginform" replace />;
-	}
+  // if token is expired, remove it
+  if (isExpired) {
+    localStorage.removeItem('token');
+    return <Navigate to="/loginform" replace />;
+  }
 
-	return <Outlet />;
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
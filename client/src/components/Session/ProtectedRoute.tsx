91b270c9be204dcc30de73a79c473ed7c8eba4e3
hskaftun <s364776@oslomet.no>
2024-02-19 13:15:49 +0100

    Fått inn noe Session elementer #2
    Sett noen videoer og fått inn redirect fra user til login-side. Login-side er foreløpig bare applikasjonen.

--- client/src/components/Session/ProtectedRoute.tsx
+++ client/src/components/Session/ProtectedRoute.tsx
@@ -1,21 +1,21 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = () => {
-	const localStorageToken = localStorage.getItem("token");
+  const localStorageToken = localStorage.getItem('token');
 
-	// decode the jwt & see if its expired
-	const decodedToken = localStorageToken
-		? JSON.parse(atob(localStorageToken.split(".")[1]))
-		: null;
-	const isExpired = decodedToken ? decodedToken.exp < Date.now() / 1000 : true;
+  // decode the jwt & see if its expired
+  const decodedToken = localStorageToken
+    ? JSON.parse(atob(localStorageToken.split('.')[1]))
+    : null;
+  const isExpired = decodedToken ? decodedToken.exp < Date.now() / 1000 : true;
 
-	// if token is expired, remove it
-	if (isExpired) {
-		localStorage.removeItem("token");
-		return <Navigate to="/loginform" replace />;
-	}
+  // if token is expired, remove it
+  if (isExpired) {
+    localStorage.removeItem('token');
+    return <Navigate to="/loginform" replace />;
+  }
 
-	return <Outlet />;
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
