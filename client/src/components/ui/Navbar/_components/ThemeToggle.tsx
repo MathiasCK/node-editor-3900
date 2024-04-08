808a84d82dda26839f27d7b0a5758d1117971027
Mathias Kolberg <Mathias.ck2000@gmail.com>
2024-04-08 14:18:50 +0200

    Improve themetoggle: Remove dropdown

--- client/src/components/ui/Navbar/_components/ThemeToggle.tsx
+++ client/src/components/ui/Navbar/_components/ThemeToggle.tsx
@@ -1,12 +1,6 @@
 import { Moon, Sun } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 import { useTheme } from '@/hooks';
 import {
   Tooltip,
@@ -22,27 +16,21 @@ const ThemeToggle = () => {
     <TooltipProvider>
       <Tooltip>
         <TooltipTrigger>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button
-                variant="outline"
-                className="border-none bg-transparent"
-                size="icon"
-              >
-                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                <span className="sr-only">Toggle theme</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem onClick={() => toggleTheme('light')}>
-                Light
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => toggleTheme('dark')}>
-                Dark
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <Button
+            variant="outline"
+            className="border-none bg-transparent"
+            size="icon"
+          >
+            <Moon
+              onClick={() => toggleTheme('dark')}
+              className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+            />
+            <Sun
+              onClick={() => toggleTheme('light')}
+              className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+            />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
         </TooltipTrigger>
         <TooltipContent>
           <p className="text-xs text-gray-500 dark:text-gray-400">
