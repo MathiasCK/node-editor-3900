58c01f9567218b7d124617647d9e3faa566a5dca
Mathias Kolberg <Mathias.ck2000@gmail.com>
2024-04-08 09:00:24 +0200

    Store all json converters in one file

---
+++ server/DAL/JsonConverters.cs
@@ -0,0 +1,91 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using server.Models;
+
+namespace server.DAL;
+
+public class AspectTypeConverter : JsonConverter<AspectType>
+{
+  public override AspectType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+  {
+    var value = reader.GetString() ?? throw new JsonException("Null, is not a valid AspectType.");
+    return value.ToLower() switch
+    {
+      "function" => AspectType.Function,
+      "product" => AspectType.Product,
+      "location" => AspectType.Location,
+      "empty" => AspectType.Empty,
+      _ => throw new JsonException($"Value '{value}' is not recognized as a valid AspectType.")
+    };
+  }
+
+  public override void Write(Utf8JsonWriter writer, AspectType value, JsonSerializerOptions options)
+  {
+    var stringValue = value switch
+    {
+      AspectType.Function => "function",
+      AspectType.Product => "product",
+      AspectType.Location => "location",
+      AspectType.Empty => "empty",
+      _ => throw new JsonException($"'{value}' is not a valid AspectType value.")
+    };
+    writer.WriteStringValue(stringValue);
+  }
+}
+
+public class EdgeTypeConverter : JsonConverter<EdgeType>
+{
+  public override EdgeType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+  {
+    var value = reader.GetString() ?? throw new JsonException("Null, is not a valid EdgeType.");
+    return value.ToLower() switch
+    {
+      "connected" => EdgeType.Connected,
+      "fulfilled" => EdgeType.Fulfilled,
+      "part" => EdgeType.Part,
+      "transfer" => EdgeType.Transfer,
+      _ => throw new JsonException($"Value '{value}' is not recognized as a valid EdgeType.")
+    };
+  }
+
+  public override void Write(Utf8JsonWriter writer, EdgeType value, JsonSerializerOptions options)
+  {
+    var stringValue = value switch
+    {
+      EdgeType.Connected => "connected",
+      EdgeType.Fulfilled => "fulfilled",
+      EdgeType.Part => "part",
+      EdgeType.Transfer => "transfer",
+      _ => throw new JsonException($"'{value}' is not a valid EdgeType value.")
+    };
+    writer.WriteStringValue(stringValue);
+  }
+}
+
+public class NodeTypeConverter : JsonConverter<NodeType>
+{
+  public override NodeType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+  {
+    var value = reader.GetString() ?? throw new JsonException("Null, is not a valid NodeType.");
+    return value.ToLower() switch
+    {
+      "block" => NodeType.Block,
+      "terminal" => NodeType.Terminal,
+      "connector" => NodeType.Connector,
+      _ => throw new JsonException($"Value '{value}' is not recognized as a valid NodeType.")
+    };
+  }
+
+  public override void Write(Utf8JsonWriter writer, NodeType value, JsonSerializerOptions options)
+  {
+    var stringValue = value switch
+    {
+      NodeType.Block => "block",
+      NodeType.Terminal => "terminal",
+      NodeType.Connector => "connector",
+      _ => throw new JsonException($"'{value}' is not a valid NodeType value.")
+    };
+    writer.WriteStringValue(stringValue);
+  }
+}
+
